b8d99cef05e00af440be1632aa9bcd5d311e9ae4
MuhammadHusnain2 <husnainprogrammer75@gmail.com>
2026-05-12 13:51:05 +0500

    Delete 01_Personal_Expense_tracker.py directory

--- 01_Personal_Expense_tracker.py/show_expense.py
+++
@@ -1,6 +0,0 @@
-#For showing information of "Expense.txt" to user
-def view_expense():
-    with open("Expense.txt","r") as f2:
-        Read = (f2.readlines())
-        for line in Read:
-            print(line.strip())
